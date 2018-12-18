c7c577019df77ca8c58f2e9ce37925feecce54eb
Sherwin Bagtas <bagtassherwin@gmail.com>
2018-12-18 08:59:33 +0700

    CaseStudy
    Edanol
    Guerrero
    Layson
    Singian

---
+++ Movers/Movers/DAL/MovieInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Movers.Models;
+
+namespace Movers.DAL
+{
+    public class MovieInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MovieContext>
+    {
+        protected override void Seed(MovieContext context)
+        {
+            var movies = new List<Movie>
+            {
+            new Movie{Title="Carson",Description="Alexander"},
+            new Movie{Title="Meredith",Description="Alonso"},
+            new Movie{Title="Arturo",Description="Anand"},
+            new Movie{Title="Gytis",Description="Barzdukas"},
+            new Movie{Title="Yan",Description="Li"},
+            new Movie{Title="Peggy",Description="Justice"},
+            new Movie{Title="Laura",Description="Norman"},
+            new Movie{Title="Nino",Description="Olivetto"}
+            };
+
+            movies.ForEach(s => context.Movies.Add(s));
+            context.SaveChanges();
+        }
+    }
+}

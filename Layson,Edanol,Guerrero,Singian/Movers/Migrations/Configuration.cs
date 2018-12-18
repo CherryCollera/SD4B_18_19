26986055bd8fe5c63baf8ae6cc08dd3150b62222
Sherwin Bagtas <bagtassherwin@gmail.com>
2018-12-18 09:59:43 +0700

    Case_Study

---
+++ Layson,Edanol,Guerrero,Singian/Movers/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Movers.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Movers.DAL.MovieContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Movers.DAL.MovieContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
